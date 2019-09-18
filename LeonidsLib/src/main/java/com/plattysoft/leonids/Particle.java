27954771d8726d7690c2a00cec1ea093b2f90adc
Valerii Kozhevnikov <valerii.kozhevnikov@balaclavalab.com>
2019-09-18 15:19:07 +0300

    Constructor argument ttl for particle.

--- LeonidsLib/src/main/java/com/plattysoft/leonids/Particle.java
+++ LeonidsLib/src/main/java/com/plattysoft/leonids/Particle.java
@@ -7,103 +7,108 @@ import com.plattysoft.leonids.modifiers.ParticleModifier;
 
 public class Particle {
 
-	protected Bitmap mImage;
+    protected Bitmap mImage;
 
-	public float mCurrentX;
-	public float mCurrentY;
+    public float mCurrentX;
+    public float mCurrentY;
 
-	public float mScale = 1f;
-	public int mAlpha = 255;
+    public float mScale = 1f;
+    public int mAlpha = 255;
 
-	public float mInitialRotation = 0f;
+    public float mInitialRotation = 0f;
 
-	public float mRotationSpeed = 0f;
+    public float mRotationSpeed = 0f;
 
-	public float mSpeedX = 0f;
-	public float mSpeedY = 0f;
+    public float mSpeedX = 0f;
+    public float mSpeedY = 0f;
 
-	public float mAccelerationX;
-	public float mAccelerationY;
+    public float mAccelerationX;
+    public float mAccelerationY;
 
-	private Matrix mMatrix;
-	private Paint mPaint;
+    private Matrix mMatrix;
+    private Paint mPaint;
 
-	private float mInitialX;
-	private float mInitialY;
+    private float mInitialX;
+    private float mInitialY;
 
-	private float mRotation;
+    private float mRotation;
 
-	private long mTimeToLive = 10000;
+    private long mTimeToLive = 10000;
 
-	protected long mStartingMilisecond;
+    protected long mStartingMilisecond;
 
-	private int mBitmapHalfWidth;
-	private int mBitmapHalfHeight;
+    private int mBitmapHalfWidth;
+    private int mBitmapHalfHeight;
 
-	private List<ParticleModifier> mModifiers;
+    private List<ParticleModifier> mModifiers;
 
-	public Rect validArea;
+    public Rect validArea;
 
-	protected Particle() {
-		mMatrix = new Matrix();
-		mPaint = new Paint();
-	}
+    protected Particle() {
+        mMatrix = new Matrix();
+        mPaint = new Paint();
+    }
 
-	public Particle (Bitmap bitmap) {
-		this();
-		mImage = bitmap;
-	}
+    public Particle(Bitmap bitmap) {
+        this();
+        mImage = bitmap;
+    }
 
-	public void init() {
-		mScale = 1;
-		mAlpha = 255;
-	}
+    public Particle(Bitmap bitmap, long ttl) {
+        this(bitmap);
+        mTimeToLive = ttl;
+    }
 
-	public void setPosition(float emiterX, float emiterY) {
-		mBitmapHalfWidth = mImage.getWidth()/2;
-		mBitmapHalfHeight = mImage.getHeight()/2;
+    public void init() {
+        mScale = 1;
+        mAlpha = 255;
+    }
 
-		mInitialX = emiterX - mBitmapHalfWidth;
-		mInitialY = emiterY - mBitmapHalfHeight;
-		mCurrentX = mInitialX;
-		mCurrentY = mInitialY;
-	}
+    public void setPosition(float emiterX, float emiterY) {
+        mBitmapHalfWidth = mImage.getWidth() / 2;
+        mBitmapHalfHeight = mImage.getHeight() / 2;
 
-	public boolean update (long miliseconds) {
-		long realMiliseconds = miliseconds - mStartingMilisecond;
-		if (realMiliseconds > mTimeToLive) {
-			return false;
-		}
-		mCurrentX = mInitialX+mSpeedX*realMiliseconds+mAccelerationX*realMiliseconds*realMiliseconds;
-		mCurrentY = mInitialY+mSpeedY*realMiliseconds+mAccelerationY*realMiliseconds*realMiliseconds;
+        mInitialX = emiterX - mBitmapHalfWidth;
+        mInitialY = emiterY - mBitmapHalfHeight;
+        mCurrentX = mInitialX;
+        mCurrentY = mInitialY;
+    }
 
-		if(validArea !=null && !validArea.contains((int)mCurrentX, (int)mCurrentY))
-			return false;
+    public boolean update(long miliseconds) {
+        long realMiliseconds = miliseconds - mStartingMilisecond;
+        if (realMiliseconds > mTimeToLive) {
+            return false;
+        }
+        mCurrentX = mInitialX + mSpeedX * realMiliseconds + mAccelerationX * realMiliseconds * realMiliseconds;
+        mCurrentY = mInitialY + mSpeedY * realMiliseconds + mAccelerationY * realMiliseconds * realMiliseconds;
 
-		//mRotation = mInitialRotation + mRotationSpeed*realMiliseconds/1000;
+        if (validArea != null && !validArea.contains((int) mCurrentX, (int) mCurrentY))
+            return false;
 
-		for (int i=0; i<mModifiers.size(); i++) {
-			mModifiers.get(i).apply(this, realMiliseconds);
-		}
+        //mRotation = mInitialRotation + mRotationSpeed*realMiliseconds/1000;
 
-		return true;
-	}
+        for (int i = 0; i < mModifiers.size(); i++) {
+            mModifiers.get(i).apply(this, realMiliseconds);
+        }
 
-	public void draw (Canvas c) {
-		mMatrix.reset();
-		if(mRotation != 0)
-			mMatrix.postRotate(mRotation, mBitmapHalfWidth, mBitmapHalfHeight);
-		if(mScale != 1)
-			mMatrix.postScale(mScale, mScale, mBitmapHalfWidth, mBitmapHalfHeight);
-		mMatrix.postTranslate(mCurrentX, mCurrentY);
-		mPaint.setAlpha(mAlpha);
-		c.drawBitmap(mImage, mMatrix, mPaint);
-	}
+        return true;
+    }
 
-	public Particle activate(long startingMilisecond, List<ParticleModifier> modifiers) {
-		mStartingMilisecond = startingMilisecond;
-		// We do store a reference to the list, there is no need to copy, since the modifiers do not carte about states 
-		mModifiers = modifiers;
-		return this;
-	}
+    public void draw(Canvas c) {
+        mMatrix.reset();
+        if (mRotation != 0)
+            mMatrix.postRotate(mRotation, mBitmapHalfWidth, mBitmapHalfHeight);
+        if (mScale != 1)
+            mMatrix.postScale(mScale, mScale, mBitmapHalfWidth, mBitmapHalfHeight);
+        mMatrix.postTranslate(mCurrentX, mCurrentY);
+        mPaint.setAlpha(mAlpha);
+        c.drawBitmap(mImage, mMatrix, mPaint);
+    }
+
+    public Particle activate(long startingMilisecond, List<ParticleModifier> modifiers) {
+        mStartingMilisecond = startingMilisecond;
+        // We do store a reference to the list, there is no need to copy, since the modifiers do not carte about states
+        mModifiers = modifiers;
+        return this;
+    }
 }
--- LeonidsLib/src/main/java/com/plattysoft/leonids/ParticleSystem.java
+++ LeonidsLib/src/main/java/com/plattysoft/leonids/ParticleSystem.java
@@ -146,7 +146,7 @@ public class ParticleSystem {
 				drawable.draw(canvas);
 			}
 
-			mParticles.add(new Particle(bitmap));
+			mParticles.add(new Particle(bitmap, ttl));
 		}
 	}
 
